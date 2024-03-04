e73343d6eff5b1cb03e2c8567f18170742d0c075
chloebchoi <144170681+chloebchoi@users.noreply.github.com>
2024-03-04 13:51:07 -0700

    Finish ex1.py

--- ex1.py
+++ ex1.py
@@ -24,22 +24,62 @@ python ex1.py '(- (* 1 3) (/ 6 (+ 1 2)))'
 '''
 import sys
 
-def arithmetic(expression): 
+class MyStack:
+    def __init__(self):
+        self.elements = []
+        
+    def push(self, element):
+        self.elements.append(element)
+        
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("Stack is empty. Cannot pop from empty stack.")
+        else:
+            return self.elements.pop()
+            
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.elements[-1]
+        
+    def is_empty(self):
+        return len(self.elements) == 0
 
-
-'''push
-top+=1
-array[top]=element value
-
-pop
-temp = array[top]
-top-=1
-'''
-if __name__=="__main__": 
+def evaluate_expression(expression):
+    stack = MyStack()
+    for char in expression:
+        if char.isdigit():
+            stack.push(int(char))
+        elif char == '(':
+            continue
+        elif char in ['+', '-', '*', '/']:
+            stack.push(char)
+        elif char == ')':
+            element1 = stack.pop()
+            element2 = stack.pop()
+            op = stack.pop()
+            if op == '+':
+                stack.push(element2 + element1)
+            elif op == '-':
+                stack.push(element2 - element1)
+            elif op == '*':
+                stack.push(element2 * element1)
+            elif op == '/':
+                stack.push(element2 // element1)
+    return stack.pop()    
+        
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("No expression given.")
         sys.exit(1)
-    
+
     expression = sys.argv[1]
-    result = arithmetic(expression)
-    print(result)
+    try:
+        result = evaluate_expression(expression)
+        print(result)
+    except ValueError as e:
+        print("Error:", e)
+
+
+    
